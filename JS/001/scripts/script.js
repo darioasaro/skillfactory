b04111362ea972164022f1227fe1dff078b6c1bf
dario <darioasaro29@gmail.com>
2020-01-23 12:23:14 -0300

    second script

--- JS/001/scripts/script.js
+++ JS/001/scripts/script.js
@@ -3,7 +3,7 @@ var anio = prompt("ingrese su año de nacimiento")
 var mes = prompt ("ingrese su mes de naciemiento")
 var dia = prompt ("ingrese su dia de nacimiento")
 
-function edad(anio,mes,dia){
+function edad(anio,mes,dia){                    //Otorgando la fecha devuelva la edad
 
 var fecha = new Date()
 var dia_hoy =  fecha.getDate()
@@ -12,18 +12,19 @@ var anio_hoy = fecha.getFullYear()
 
 var edad = anio_hoy - anio - 1
 
-if((mes_hoy - mes) > 0){
+if((mes_hoy - mes) > 0){                    //compara meses para saber si ya cumpliste años
     edad +=1
 }
 
-if ((mes_hoy - mes) == 0){
+if ((mes_hoy - mes) == 0){                  //si estamos en el mismo mes, luego comprobara los dias
     
-    if((dia_hoy - dia) > 0){
+    if((dia_hoy - dia) > 0){                 //compara dias para saber si ya paso el cumpleaños
         edad +=1
     }
-    if(dia_hoy == dia){
+    else if(dia_hoy == dia){                 //si es el mismo dia, felicita
         edad +=1
-        console.log("Feliz Cumpleaños! hoy cumplis " edad)
+        console.log("Feliz Cumpleaños! hoy cumplis " + edad + "años")
+        alert("FELIZ CUMPLEAÑOS "+ nombre + "!!!")
     }
 }
 
@@ -35,6 +36,7 @@ var xx = edad(anio,mes,dia)
 
 console.log(xx)
 
+ 
 
  
  
